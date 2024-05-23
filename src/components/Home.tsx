60cf33f5cb7029536f671cbeafd1a7e795d22abb
Ahsanul Habib <ahsanulhabib1910675@gmail.com>
2024-05-24 02:13:29 +0600

    address changed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -41,7 +41,7 @@ const AirWarning=
     const Quote=getRandomItem(DailyQuotes);
     // let largestNumber=0;
     const fetchData=async ()=>{
-        const data=await axios.get("https://aircampus-backend.onrender.com/");
+        const data=await axios.get("https://aircampus-backend2.onrender.com");
         const mainData=data.data;
         console.log(mainData);
         const arr = Object.entries(mainData as {[key: string]: any });
